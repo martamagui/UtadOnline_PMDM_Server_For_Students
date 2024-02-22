ae1f330459dcde6d55093bca21013f587f97595b
martamagui <mmolina@digital-fruit.com>
2024-02-22 12:46:17 +0100

    Logs

--- server/middleware/userExtractor.ts
+++ server/middleware/userExtractor.ts
@@ -10,14 +10,23 @@ export const userExtractor = async (
   try {
     const auth = req.headers.authorization;
     if (auth == null) {
-      return res.status(401).json({ message: "You need to add the authorization in the request header." });
-    }
-    if (auth.toLowerCase().startsWith("bearer")) {
+      return res
+        .status(401)
+        .json({
+          message: "You need to add the authorization in the request header.",
+        });
+    } else {
       const token = auth.trim();
       if (auth != null && auth.length > 0) {
-        console.log(auth);
+        let time = formatedDate()
+        let ip = req.ip
+        console.log(auth + ",  Fecha: (" + time+ ") IP: " + ip);
       } else {
-        return res.status(401).json({ message: "You need to add the authorization in the request header." });
+        return res
+          .status(401)
+          .json({
+            message: "You need to add the authorization in the request header.",
+          });
       }
     }
   } catch (error) {
@@ -26,3 +35,24 @@ export const userExtractor = async (
   }
   next();
 };
+
+const formatedDate = ()=>{
+
+const fechaActual: Date = new Date();
+
+const dia: number = fechaActual.getDate();
+const mes: number = fechaActual.getMonth() + 1; // ¡Recuerda que los meses van de 0 a 11!
+const año: number = fechaActual.getFullYear();
+const horas: number = fechaActual.getHours();
+const minutos: number = fechaActual.getMinutes();
+
+const diaStr: string = dia < 10 ? '0' + dia : '' + dia;
+const mesStr: string = mes < 10 ? '0' + mes : '' + mes;
+const horasStr: string = horas < 10 ? '0' + horas : '' + horas;
+const minutosStr: string = minutos < 10 ? '0' + minutos : '' + minutos;
+
+// Formatear la fecha como DD/MM/YYYY HH:mm
+const fechaFormateada: string = `${diaStr}/${mesStr}/${año} ${horasStr}:${minutosStr}`;
+
+return fechaFormateada;
+}
